717676a2cbaf5556fb68529e3b3f81669befb926
Krishna2nd <krishna.kumar@walmartlabs.com>
2019-05-24 08:33:33 +0530

    Remove commented code

--- screens/Cart/cart.actions.js
+++ screens/Cart/cart.actions.js
@@ -109,16 +109,7 @@ export const addToCart = product => (dispatch, getState) => {
       list: cart
     })
     .then(() => {
-      // docCart.get().then(function(doc) {
-      //   if (doc.exists) {
-      //     console.log("Document data:", doc.data());
-      //   } else {
-      //     // doc.data() will be undefined in this case
-      //     console.log("No such document!");
-      //   }
-      // });
     })
     .catch(function(error) {
-      // console.error("Error adding document: ", error);
     });
 };
